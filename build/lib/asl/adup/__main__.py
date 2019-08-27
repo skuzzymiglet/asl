dcb6c0c2e4071dc35cdad6b1c75256355b179423
skuzzymiglet <skuzzymiglet@gmail.com>
2019-08-27 20:38:00 +0100

    absolute path for README

--- build/lib/asl/adup/__main__.py
+++
@@ -1,9 +0,0 @@
-import os, tempfile
-
-def dup_folder(path):
-    tmp = tempfile.TemporaryDirectory()
-    for i in os.listdir(path):
-        for n in range(5):
-            os.symlink(path+i, tmp.name+"/"+i.split(".")[0]+str(n)+"."+i.split(".")[1])
-    return tmp
-
