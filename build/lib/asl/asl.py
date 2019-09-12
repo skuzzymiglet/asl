c9fe821ed0cf462b7e81d8684fa5561f2bb78ca0
skuzzymiglet <skuzzymiglet@gmail.com>
2019-09-12 17:54:53 +0100

    fixed indents

--- build/lib/asl/asl.py
+++ build/lib/asl/asl.py
@@ -6,10 +6,11 @@ import os
 import sys
 
 if sys.platform == "win32":
-   import pyautogui
+    import pyautogui
+    from ctypes import windll
 
-   user32 = windll.user32
-   user32.SetProcessDPIAware()
+    user32 = windll.user32
+    user32.SetProcessDPIAware()
 
 name = "asl"
 INTERVAL = 15
